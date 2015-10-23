64bc53e6e2cf8ded56b1615d77eba27ec9159b01
Ty Overby <ty@pre-alpha.com>
2015-10-23 08:40:40 -0700

    add svg tests

--- src/svg.rs
+++ src/svg.rs
@@ -20,12 +20,18 @@ struct SvgWriter {
     id_gen: u32,
 }
 
+impl SvgWriter {
+    fn new() -> SvgWriter {
+        SvgWriter {
+            defs: vec![],
+            body: vec![],
+            id_gen: 0
+        }
+    }
+}
+
 pub fn write_svg<W: Write>(drawing: &Drawing, writer: &mut W) -> IoResult<()> {
-    let mut svg_writer = SvgWriter {
-        defs: Vec::new(),
-        body: Vec::new(),
-        id_gen: 0
-    };
+    let mut svg_writer = SvgWriter::new();
 
     for figure in drawing.figures() {
         write_figure(figure, &mut svg_writer);
@@ -56,4 +62,47 @@ fn write_figure(figure: &Figure, writer: &mut SvgWriter) {
                 x, y, radius, width, fill.map(|a| a.to_string()).unwrap_or_else(|| "none".into()))
         }
     }).unwrap();
+}
+
+
+#[cfg(test)]
+mod test {
+    use ::{Unit, Figure};
+
+    fn test_figure(fig: &Figure) -> String {
+        use std::io::Write;
+        use super::SvgWriter;
+
+        let mut writer = SvgWriter::new();
+        super::write_figure(fig, &mut writer);
+        let mut buf = vec![];
+        buf.write_all(&writer.body[..]).unwrap();
+        String::from_utf8(buf).unwrap()
+    }
+
+    #[test]
+    fn test_line() {
+        let line = Figure::Line {
+            p1: (Unit::Px(5.0),  Unit::Cm(10.0)),
+            p2: (Unit::Mm(20.0), Unit::In(40.0)),
+            width: Unit::Px(50.0),
+        };
+        assert_eq!(
+            &test_figure(&line),
+            r#"<line stroke="black" x1="5px" y1="10cm" x2="20mm" y2="40in" stroke-width="50px"/>"#);
+    }
+
+    #[test]
+    fn test_circle() {
+        let circle = Figure::Circle {
+            center: (Unit::Px(5.0),  Unit::Cm(10.0)),
+            radius: Unit::In(15.0),
+            width: Unit::Px(50.0),
+            fill: None
+        };
+
+        assert_eq!(
+            &test_figure(&circle),
+            r#"<circle cx="5px" cy="10cm" r="15in" stroke-width="50px" stroke="black" fill="none" />"#)
+    }
 }
